6c3d121adbf18511c539ae0c5a5eac23e5af29ae
rivalsaja <rivaldikls9@gmail.com>
2024-06-13 08:54:55 +0700

    Sudah dikerjakan sampai pert-12 membuat CRUD DATA BUKU

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -31,6 +31,12 @@
                                 <i class="fa fa-fw fa book"></i>
                                 <span>Data Anggota</span></a>
                         </li>
+                         <!-- Nav Item - Dashboard -->
+                         <li class="nav-item">
+                            <a class="nav-link pb-0" href="<?= base_url('buku/kategori'); ?>">
+                                <i class="fa fa-fw fa book"></i>
+                                <span>Kategori</span></a>
+                        </li>
                     </li>
                 <!-- Divider -->
                 <hr class="sidebar-divider mt-3">
